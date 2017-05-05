adedf5f94f1b0248f4dd647c0b6b4c7f0c427467
Jonathan Seubert <finisher1017@gmail.com>
2017-05-04 21:07:17 -0700

    3rd analytics push

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module SeubsFinal
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.paths << "#{Rails.root}/app/assets/videos"
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
